05f37a39f40f846f0143e05e75ddf57085776dee
nathalia ;* <162830525+nrbianchini@users.noreply.github.com>
2025-03-01 20:47:20 -0300

    Update InchesToCentimeters.java

--- src/sequentialStructure/InchesToCentimeters.java
+++ src/sequentialStructure/InchesToCentimeters.java
@@ -1,13 +1,15 @@
-package sequentialStructure;
+/* Reads a measurement in inches and prints out the equivalent in centimeters, 
+   knowing that 2.54 cm equals 1 inch. */
 
+package sequentialStructure;
 import java.util.Scanner;
 
 public class InchesToCentimeters {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        System.out.print("Insira a medida em polegadas: ");
+        System.out.print("Enter the measurement in inches: ");
         double inches =  input.nextDouble();
-        System.out.print("A medida em centímetros é: " + (inches * 2.54));
+        System.out.print("The measurement in centimeters is: " + (inches * 2.54));
     }
 }
