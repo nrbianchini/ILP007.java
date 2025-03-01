0fb3eda500b4eb2384870bf7b9556ae718724734
nathalia ;* <162830525+nrbianchini@users.noreply.github.com>
2025-03-01 20:49:20 -0300

    Update CarCostCalculator.java

--- src/sequentialStructure/CarCostCalculator.java
+++ src/sequentialStructure/CarCostCalculator.java
@@ -1,3 +1,8 @@
+/* The cost of a new car to the consumer is the sum of the factory cost, the distributor's 
+percentage, and the taxes (applied to the factory cost). Assuming the distributor's percentage 
+is 28% and the taxes are 45%, write an algorithm to read the factory cost of a car, calculate, 
+and print the final cost to the consumer. */
+
 package sequentialStructure;
 
 import java.util.Scanner;
@@ -6,10 +11,10 @@ public class CarCostCalculator {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        System.out.print("Insira o custo de fábrica do veículo: R$");
+        System.out.print("Enter the factory cost of the vehicle: R$");
         double factoryCost = input.nextDouble();
 
         double percentageTD = (factoryCost * 0.28) +  (factoryCost * 0.45);
-        System.out.println("O custo final ao consumidor é de: R$" + (factoryCost + percentageTD));
+        System.out.println("The final cost to the consumer is: R$" + (factoryCost + percentageTD));
     }
 }
